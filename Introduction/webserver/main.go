30c29fcf6e412ce389790d6612c6f4781c07e9cf
david gomez <dgomezda@gmail.com>
2020-06-16 23:57:33 -0400

    Adding basic mvc structure

---
+++ Introduction/webserver/main.go
@@ -0,0 +1,10 @@
+package main
+
+//func main(){
+//	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+//		writer.Write([]byte("Hello"))
+//	})
+//	if err:= http.ListenAndServe(":8080", nil);err != nil {
+//		panic(err)
+//	}
+//}
